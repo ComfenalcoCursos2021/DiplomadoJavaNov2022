a640da7b3e369c3ce05af3fd17ff918d3b7f6563
alvaro.obregon <alvaro911>
2022-12-09 19:39:35 -0500

    test RestApi en DiplomadoS4

--- DIPLOMADO_S4/TestApiRest/src/com/alv/main/Principal.java
+++ DIPLOMADO_S4/TestApiRest/src/com/alv/main/Principal.java
@@ -13,42 +13,52 @@ public class Principal {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+
+		String respuestaSaludo = RestUtil
+				.urlConnectionGetRequest("http://localhost:8080/HotelService-0.0.1-SNAPSHOT/test/saludo");
+		System.out.println("---> " + respuestaSaludo);
+
+		try {
+			String respuesta = RestUtil.urlConnectionGetRequest("http://localhost:8080/persona/obtenerPrimero");
+			ConvertirUno(respuesta);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		
-		String respuesta = RestUtil.urlConnectionGetRequest("http://localhost:8080/persona/obtenerPrimero");
-		ConvertirUno(respuesta);
-		
-		respuesta = RestUtil.urlConnectionGetRequest("http://localhost:8080/persona/listar");
-		ConvertirTodos(respuesta);
+		try {
+			String respuesta = RestUtil.urlConnectionGetRequest("http://localhost:8080/persona/listar");
+			ConvertirTodos(respuesta);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		System.out.println("Termino");
 	}
-	
-	
-	
-	
+
 	public static void ConvertirUno(String contenido) {
 		Gson gson = new Gson();
 		PersonaQueViene persona = gson.fromJson(contenido, PersonaQueViene.class);
 		System.out.println(persona);
-		
+
 	}
+
 	public static void ConvertirTodos(String contenido) {
 		Gson gson = new Gson();
-		
+
 		List<PersonaQueViene> personas = (ArrayList<PersonaQueViene>) fromJson(contenido,
-                new TypeToken<ArrayList<PersonaQueViene>>() {
-                }.getType());
-		
+				new TypeToken<ArrayList<PersonaQueViene>>() {
+				}.getType());
+
 		for (PersonaQueViene persona : personas) {
-			
+
 			System.out.println(persona);
 			System.out.println("---------------------------");
-			
+
 		}
-		
+
 	}
+
 	public static Object fromJson(String jsonString, Type type) {
-	    return new Gson().fromJson(jsonString, type);
+		return new Gson().fromJson(jsonString, type);
 	}
-	
 
 }
